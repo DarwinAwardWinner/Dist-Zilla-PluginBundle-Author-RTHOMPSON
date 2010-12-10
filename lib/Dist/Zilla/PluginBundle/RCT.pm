76f9ca80796fbc837192ee4e5bfe6f5bbb86eef9
Ryan C. Thompson <ryan@aeolus>
2010-12-10 01:07:10 -0800

    First configurable version

--- lib/Dist/Zilla/PluginBundle/RCT.pm
+++ lib/Dist/Zilla/PluginBundle/RCT.pm
@@ -9,12 +9,133 @@ package Dist::Zilla::PluginBundle::RCT;
 use Moose;
 with 'Dist::Zilla::Role::PluginBundle::Easy';
 
-=for Pod::Coverage  configure
+my $sample_dist_ini = <<'EOF';
+fdfsd
+[@RCT]
+; Support remove like @Filter
+-remove = SynopsisTests
+-remove = JRandomPlugin
 
-=cut
+; Use AutoVersion
+version = auto
+version_major = 0
+; OR Use manual versioning
+; TODO: Write Dist::Zilla::Plugin::StaticVersion
+version = 1.14.04
+; OR Provide no version
+
+; Whether to do real releases
+release = real (default)
+; OR fake
+release = fake
+; OR other
+release = SpecificPluginName
+
+; Whether to archive
+archive = yes|true|1 (default)
+; OR not
+archive = no|false|0|-1
+
+; Whether the synopsis is perl code that should be tested for successful compilation
+synopsis_is_perl_code = true
+
+; Files to copy from build dir
+copy_file = README
+copy_file = README.mkdn
+
+EOF
+
+sub mvp_multivalue_args { qw( -remove copy_file ) }
+
+my @options = qw( -remove version version_major synopsis_is_perl_code copy_file
+                  release archive  );
+
+# Returns true for strings of 'true', 'yes', or positive numbers,
+# false otherwise.
+sub _parse_bool {
+    return 1 if ($_ > 1 or m{true|yes}i);
+}
+
 
 sub configure {
     my $self = shift;
+    state $defaults = {
+        # AutoVersion by default
+        version => 'auto',
+        # Assume that the module is experimental unless told
+        # otherwise.
+        version_major => 0,
+        # Assume that synopsis is perl code and should compile
+        # cleanly.
+        synopsis_is_perl_code => 1,
+        # Realease to CPAN for real
+        release => 'real',
+        # Archive releases
+        archive => 1,
+        archive_directory => 'releases',
+        # Copy README.pod from build dir to dist dir, for Github and
+        # suchlike.
+        copy_file => [ 'README.pod' ],
+    };
+    my %args = (%$defaults %{$self->payload});
+
+    # Use the @Filter bundle to handle '-remove'.
+    if ($args{-remove}) {
+        $self->add_bundle('@Filter' => { %args, -bundle => '@RCT' });
+        return;
+    }
+
+    # Add appropriate version plugin
+    if ($args{version} eq 'auto') {
+        $self->add_plugins(
+            [ 'AutoVersion' => { major => $args{version_major} } ]
+        );
+    }
+    else {
+        $self->add_plugins(
+            [ 'StaticVersion' => { version => $args{version} } ]
+        );
+    }
+    # TODO support other version providers
+
+    # Copy files from build dir
+    $self->add_plugins(
+        [ 'CopyFilesFromBuild' => {
+            file => $args{copy_file}
+        }]
+    );
+
+    # Decide whether to test SYNOPSIS for syntax.
+    if (_parse_bool($args{synopsis_is_perl_code})) {
+        $self->add_plugins('SynopsisTests');
+    }
+
+    # Choose release plugin
+    given ($args{release}) {
+        when (lc eq 'real') {
+            $self->add_plugins('UploadToCPAN')
+        }
+        when (lc eq 'fake') {
+            $self->add_plugins('FakeRelease')
+        }
+        when ($_) {
+            $self->add_plugins($_)
+        }
+        default {
+            # Empty string means no release plugin
+        }
+    }
+
+    # Choose whether and where to archive releases
+    if (_parse_bool($args{archive})) {
+        $self->add_plugins(
+            ['ArchiveRelease' => {
+                directory => $args{archive_directory},
+            }]
+        );
+    }
+
+    # All the invariant plugins
     $self->add_plugins(
         # @Basic
         'GatherDir',
@@ -29,17 +150,21 @@ sub configure {
 
         # Mods
         'PkgVersion',
+        # TODO: Only add PodWeaver if weaver.ini exists
         'PodWeaver',
 
         # Generated Docs
         'InstallGuide',
-        'ReadmeFromPod',
-        'ReadmeMarkdownFromPod',
-        ['CopyFilesFromBuild' => {
-            # This is for GitHub and similar things the expect a
-            # README under version control.
-            file => [ 'README.mkdn' ],
+        ['ReadmeAnyFromPod', 'text', {
+            filename => 'README',
+            type => 'text',
         }],
+        ['ReadmeAnyFromPod', 'pod', {
+            filename => 'README.pod',
+            type => 'pod',
+        }],
+
+        # This can't hurt. It's a no-op if github is not involved.
         'GithubMeta',
 
         # Tests
@@ -47,9 +172,11 @@ sub configure {
         'PodTests',
         'HasVersionTests',
         'PortabilityTests',
-        'SynopsisTests',
         'UnusedVarsTests',
         ['CompileTests' => {
+            # The test files don't seem to compile in the context of
+            # this test. But it's ok, because if they really have
+            # problems, they'll fail to compile when they run.
             skip => 'Test$',
         }],
         'KwaliteeTests',
@@ -67,10 +194,6 @@ sub configure {
         'NextRelease',
         'TestRelease',
         'ConfirmRelease',
-        'ArchiveRelease',
-
-        # 'FakeRelease',
-        'UploadToCPAN',
 
     );
 }
@@ -82,10 +205,13 @@ __END__
 In dist.ini:
 
     [@RCT]
+    TODO ADD STUFF
 
 =head1 DESCRIPTION
 
-See the code. It lists all the plugins that it loads quite clearly.
+TODO ADD STUFF
+
+=for Pod::Coverage  configure mvp_multivalue_args
 
 =head1 BUGS AND LIMITATIONS
 
