6977683d9b9a17f7eb4f5fc95ce4a22c939d9cd4
Ryan C. Thompson <rct@thompsonclan.org>
2012-01-15 16:56:36 -0800

    Fix warning text

--- lib/Dist/Zilla/PluginBundle/RTHOMPSON.pm
+++ lib/Dist/Zilla/PluginBundle/RTHOMPSON.pm
@@ -11,7 +11,7 @@ extends "Dist::Zilla::PluginBundle::Author::RTHOMPSON";
 use namespace::autoclean;
 
 before register_component => sub {
-  warn '!!! [@RTHOMPSON] is deprecated and may be removed in the future; replace it with [@Author::RTHOMPSON]\n';
+  warn "!!! [\@RTHOMPSON] is deprecated and may be removed in the future; replace it with [\@Author::RTHOMPSON]\n";
 };
 
 1; # Magic true value required at end of module
